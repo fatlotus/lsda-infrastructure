ed277fc9b6c3c89c39da4957b0af13cb1c0a70be
Jeremy Archer <open-source@fatlotus.com>
2013-12-03 15:43:22 -0600

    Simplify set up of Gitolite.

--- servers/gitserver.sh
+++ servers/gitserver.sh
@@ -6,28 +6,30 @@
 # Date: 3 December 2013.
 #
 
-set -e
-set -x
+set -e -x
 
 # Prepare the system for install.
-useradd -m -r git
-aptitude install git python
+useradd -m -r git || true
+DEBIAN_FRONTEND=noninteractive apt-get install -y git python
+
+cd /home/git
 
 su - git <<EOF
 
-# Include my public key in the setup process.
+set -e -x
 
-cat > spectre-secure.pub <<NEOF
+# Include my public key in the setup process.
+cat > jeremy.pub <<NEOF
 ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCpZxfYDGv07zdwv2Qa7SLIUi74rhR6D7y41uVbo6SUn3EbJbc7rSgO+CTgQTbbTNn9eEjMxQ3Q2QRt9FdyTRT6Gn607gtqi1uKkuRkkIxDTGNmRgrbC55M8Q07u8Z1PJvOmd5MnP3Et2dQdVJ1UCawNQLuktPup7sLBafsPHiDUnjTrI61x0E2fnyXOOlr1NmLWYQ4Plp8lQhOF9Y1hx8et3k0/y/r/TaVa5VI12kJ11f0vvvaZhAqKdbEKZE1XOYVD8ZrH8qs/x1a807Ilcvvr6LJ0TnZ3aUYL80cBT3IbCNwDgqytGXMCyBh9KTnjMB4neIkUYGkiyCDfzhQPzyr spectre-secure
 NEOF
 
 # Fetch and Install Gitolite
-git clone git://github.com/sitaramc/gitolite
-mkdir -p ./bin
-gitolite/install -to ./bin
-bin/gitolite setup -pk spectre-secure.pub
+git clone git://github.com/sitaramc/gitolite || true
+gitolite/src/gitolite setup -pk jeremy.pub
 
 # Clean up
-rm -rf spectre-secure.pub
+rm -rf jeremy.pub
 
 EOF
+
+echo "Installation complete!"
