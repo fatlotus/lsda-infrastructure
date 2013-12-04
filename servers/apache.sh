c6329fa73f873d0bd0917d215325bf9ac7cff9fc
Jeremy Archer <open-source@fatlotus.com>
2013-12-04 01:01:03 -0600

    Add automated commits.

--- servers/apache.sh
+++ servers/apache.sh
@@ -17,13 +17,14 @@ DEBIAN_FRONTEND=noninteractive apt-get -y install apache2 \
 cat > /etc/apache2/httpd.conf <<EOF
 LDAPTrustedGlobalCert CA_BASE64 /etc/ssl/certs/ca-certificates.crt
 LogLevel debug
+User git
 
 <Location />
   AuthBasicProvider ldap
   AuthType Basic
   AuthName "Restricted Area"
   AuthLDAPURL "ldaps://ldap.uchicago.edu/ou=people,dc=uchicago,dc=edu?uid?one" STARTTLS
-  Require valid-user
+  Require user jarcher
   
   Options ExecCGI
   AddHandler cgi-script .cgi
@@ -43,7 +44,6 @@ chmod 0400 /etc/lighttpd/allowed_users.list
 a2enmod authnz_ldap
 
 curl https://raw.github.com/fatlotus/lsda-installation/master/generate-ssh-key.cgi > /var/www/generate-ssh-key.cgi
-chmod u+x /var/www/generate-ssh-key.cgi
-chown git /var/www/generate-ssh-key.cgi
+chmod +x /var/www/generate-ssh-key.cgi
 
 /etc/init.d/apache2 restart || /etc/init.d/apache2 restart
--- servers/gitserver.sh
+++ servers/gitserver.sh
@@ -24,12 +24,20 @@ cat > jeremy.pub <<NEOF
 ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCpZxfYDGv07zdwv2Qa7SLIUi74rhR6D7y41uVbo6SUn3EbJbc7rSgO+CTgQTbbTNn9eEjMxQ3Q2QRt9FdyTRT6Gn607gtqi1uKkuRkkIxDTGNmRgrbC55M8Q07u8Z1PJvOmd5MnP3Et2dQdVJ1UCawNQLuktPup7sLBafsPHiDUnjTrI61x0E2fnyXOOlr1NmLWYQ4Plp8lQhOF9Y1hx8et3k0/y/r/TaVa5VI12kJ11f0vvvaZhAqKdbEKZE1XOYVD8ZrH8qs/x1a807Ilcvvr6LJ0TnZ3aUYL80cBT3IbCNwDgqytGXMCyBh9KTnjMB4neIkUYGkiyCDfzhQPzyr spectre-secure
 NEOF
 
+# Wipe out any existing keys (since we mess with this file later)
+rm -rf ~/.ssh/authorized_keys
+
 # Fetch and Install Gitolite
 git clone git://github.com/sitaramc/gitolite || true
 gitolite/src/gitolite setup -pk jeremy.pub
 
 # Install a password for the CGI script to connect to itself
-ssh-keygen -t rsa -f ~/.ssh/id_rsa -P ""
+yes | ssh-keygen -t rsa -f ~/.ssh/id_rsa -P ""
+
+# Trust the local Git SSH key.
+echo -n 'command="/home/git/gitolite/src/gitolite-shell git",' > ~/.ssh/authorized_keys2
+echo -n 'no-port-forwarding,no-X11-forwarding,no-agent-forwarding,no-pty ' >> ~/.ssh/authorized_keys2
+cat /home/git/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys2
 
 # Clean up
 rm -rf jeremy.pub
