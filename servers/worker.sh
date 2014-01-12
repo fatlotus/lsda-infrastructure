62d68e49a4f8abaef37db4bbe791f35436199e51
Jeremy Archer <open-source@fatlotus.com>
2014-01-12 14:47:52 -0600

    Update worker configuration.

--- servers/worker.sh
+++ servers/worker.sh
@@ -12,6 +12,7 @@ set -e -x
 useradd -m -r lsda || true
 useradd -m -r sandbox || true
 DEBIAN_FRONTEND=noninteractive apt-get update
+DEBIAN_FRONTEND=noninteractive apt-get upgrade -y
 DEBIAN_FRONTEND=noninteractive apt-get install -y git python python-distribute \
   python-pip build-essential python-dev
 
@@ -20,14 +21,31 @@ rm -rf /worker || true
 mkdir -p /worker
 cd /worker
 git clone https://github.com/fatlotus/lsda-management.git .
+
+# Allow lsda to jump into the sandbox.
+cat > /etc/sudoers.d/lsda <<EOF
+lsda ALL=(root) NOPASSWD: /worker/sandbox.py *
+EOF
+chmod 0440 /etc/sudoers.d/lsda
+
 pip install -r /worker/requirements.txt
 chown -R lsda:lsda .
 
 # Configure security.
+cat > /etc/network/if-pre-up.d/sandbox-firewall <<EOF
+#!/bin/bash
+iptables -F
 iptables -A OUTPUT -m state -m owner --state ESTABLISHED,RELATED \
-  --uid-owner sandbox -j REJECT
-iptables -A OUTPUT -m owner --uid-owner sandbox -j REJECT
+  --uid-owner sandbox -j ACCEPT
+iptables -A OUTPUT -m owner --uid-owner sandbox -p tcp --dport 1024:65535 \
+  -d 0.0.0.0 -j ACCEPT
+iptables -A OUTPUT -m owner --uid-owner sandbox -p tcp --dport 1024:65535 \
+  -d 127.0.0.1 -j ACCEPT
+EOF
+chmod +x /etc/network/if-pre-up.d/sandbox-firewall
+/etc/network/if-pre-up.d/sandbox-firewall
 
+# Configure daemons
 cat > /etc/init/lsda.conf <<EOF
 description "Runs an Python LSDA Worker Process"
 author "Jeremy Archer <jarcher@uchicago.edu>"
