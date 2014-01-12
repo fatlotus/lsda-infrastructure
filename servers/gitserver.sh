8fbcd8377d347f3e37f5dd16e7bb7386d69abd8e
Jeremy Archer <open-source@fatlotus.com>
2014-01-12 14:47:44 -0600

    Update Git configuration.

--- servers/gitserver.sh
+++ servers/gitserver.sh
@@ -11,7 +11,8 @@ set -e -x
 # Prepare the system for install.
 useradd -m -r git || true
 DEBIAN_FRONTEND=noninteractive apt-get update
-DEBIAN_FRONTEND=noninteractive apt-get install -y git python php5
+DEBIAN_FRONTEND=noninteractive apt-get install -y git python php5 python-pip \
+  build-essential python-dev libcap-dev
 
 cd /home/git
 
@@ -21,10 +22,10 @@ git clone https://github.com/fatlotus/lsda-management.git
 pip install -r lsda-management/submitter_requrements.txt 2>/dev/null
 
 # Install Gitlist
-rm -rf /var/www/gitlist || true
+rm -rf /var/gitlist || true
 wget https://s3.amazonaws.com/gitlist/gitlist-0.4.0.tar.gz
-tar xvf gitlist-0.4.0.tar.gz -C /var/www
-cat > /var/www/gitlist/config.ini <<EOF
+tar xvf gitlist-0.4.0.tar.gz -C /var
+cat > /var/gitlist/config.ini <<EOF
 [git]
 client = '/usr/bin/git';
 default_branch = 'master';
@@ -39,17 +40,7 @@ timezone = CST
 EOF
 
 # Ensure that Gitlist works in a subdirectory.
-cat > /var/www/gitlist/.htaccess <<EOF
-
-<IfModule mod_rewrite.c>
-  Options -MultiViews SymLinksIfOwnerMatch
-
-  RewriteEngine On
-  RewriteBase /gitlist
-
-  RewriteCond %{REQUEST_FILENAME} !-f
-  RewriteRule ^(.*)$ index.php [L,NC]
-</IfModule>
+cat > /var/gitlist/.htaccess <<EOF
 
 <Files config.ini>
   Order Allow,Deny
@@ -57,9 +48,9 @@ cat > /var/www/gitlist/.htaccess <<EOF
 </Files>
 
 EOF
-mkdir -p /var/www/gitlist/cache
-chown git:git /var/www/gitlist/cache
-chmod 0777 /var/www/gitlist/cache
+mkdir -p /var/gitlist/cache
+chown git:git /var/gitlist/cache
+chmod 0777 /var/gitlist/cache
 rm gitlist-0.4.0.tar.gz
 
 su - git <<EOF
@@ -78,14 +69,25 @@ rm -rf ~/.ssh/authorized_keys*
 rm -rf gitolite
 git clone git://github.com/sitaramc/gitolite
 
-# Create a triggering post-receive hook.
-mkdir -p ~/.gitolite/hooks
-rm -rf ~/.gitolite/hooks/common/post-update
+# Create a triggering post-receive VREF.
 ln -s /home/git/lsda-management/submitter.py \\
-  ~/.gitolite/hooks/common/post-update
+  ~/gitolite/src/VREF/submit_to_lsda
 
-# Install Gitolite globally.
+# Install a quota update VREF.
+cat > ~/gitolite/src/VREF/quota_copier <<EOF2
+#!/bin/bash -ex
+
+python /home/git/lsda-management/quota_copier.py \\
+  --zookeeper zookeeper.lsda.cs.uchicago.edu \\
+  --config /home/git/.gitolite/conf/quotas.yaml
+
+EOF2
+chmod +x ~/gitolite/src/VREF/quota_copier
+
+# Install Gitolite globally the second time.
+#   Trust me on this, I'm from the past. This is the only way.
 gitolite/src/gitolite setup -pk jeremy.pub
+rm -rf /home/git/repositories/testing.git
 
 # Install a password for the CGI script to connect to itself
 yes | ssh-keygen -t rsa -f ~/.ssh/id_rsa -P ""
@@ -94,6 +96,11 @@ yes | ssh-keygen -t rsa -f ~/.ssh/id_rsa -P ""
 echo -n 'command="/home/git/gitolite/src/gitolite-shell git",' > ~/.ssh/authorized_keys2
 echo -n 'no-port-forwarding,no-X11-forwarding,no-agent-forwarding,no-pty ' >> ~/.ssh/authorized_keys2
 cat /home/git/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys2
+
+# Install the add_response.cgi script.
+rm -rf /home/git/repositories/add_response.cgi
+cp lsda-management/add_response.cgi /home/git/repositories
+chmod +x /home/git/repositories/add_response.cgi
 
 # Clean up
 rm -rf jeremy.pub
