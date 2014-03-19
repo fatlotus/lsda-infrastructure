8ab71854d5ab2a51a7067f4b6b91d7c3daebd936
Jeremy Archer <open-source@fatlotus.com>
2014-03-19 16:40:20 -0500

    Add control panel.

--- servers/apache.sh
+++ servers/apache.sh
@@ -11,7 +11,8 @@ set -e -x
 # Prepare system for install.
 DEBIAN_FRONTEND=noninteractive apt-get update
 DEBIAN_FRONTEND=noninteractive apt-get -y install apache2 \
-  pwauth libapache2-mod-authnz-external libpam-ldap
+  pwauth libapache2-mod-authnz-external libpam-ldap python-pip \
+  libapache2-mod-wsgi git-core
 
 # Configure Apache2.
 cat > /etc/apache2/httpd.conf <<EOF
@@ -26,28 +27,22 @@ Listen 0.0.0.0:1337
   SSLCertificateKeyFile /etc/apache2/ssl/apache.key
   ServerName lsda.cs.uchicago.edu
   
-  DocumentRoot /var/www
+  WSGIDaemonProcess myapplication
+  WSGIScriptAlias / /control-panel/app.wsgi
+  
+  ScriptAlias /cgi-bin /var/www
   
   <Location />
     AuthBasicProvider ldap
     AuthType Basic
     AuthName "CNetID"
     AuthLDAPURL "ldaps://ldap.uchicago.edu/ou=people,dc=uchicago,dc=edu?uid?one" STARTTLS
-    Require user jarcher cioc lafferty borja howens
-    
-    Options ExecCGI
-    AddHandler cgi-script .cgi
+    Require user jarcher cioc lafferty
   </Location>
   
-  Alias /grading /var/gitlist
-  
-  <Location /grading>
-    Require user jarcher lafferty
-    Options +FollowSymLinks +SymLinksIfOwnerMatch
-    
-    RewriteEngine on
-    RewriteCond %{REQUEST_FILENAME} !-f
-    RewriteRule ^(.*)$ index.php
+  <Location /cgi-bin>
+    Options ExecCGI
+    AddHandler cgi-script .cgi
   </Location>
 </VirtualHost>
 
@@ -58,6 +53,17 @@ Listen 0.0.0.0:1337
   AddHandler cgi-script .cgi
 </VirtualHost>
 EOF
+
+# Alias /grading /var/gitlist
+# 
+# <Location /grading>
+#   Require user jarcher lafferty
+#   Options +FollowSymLinks +SymLinksIfOwnerMatch
+#   
+#   RewriteEngine on
+#   RewriteCond %{REQUEST_FILENAME} !-f
+#   RewriteRule ^(.*)$ index.php
+# </Location>
 
 # Configure LDAP.
 cat > /etc/ldap.conf <<EOF
@@ -75,5 +81,10 @@ mkdir -p /var/www
 
 curl https://raw.github.com/fatlotus/lsda-installation/master/generate-ssh-key.cgi > /var/www/generate-ssh-key.cgi
 chmod +x /var/www/generate-ssh-key.cgi
+
+# Configure the LSDA control panel.
+rm -rf /control-panel
+git clone https://github.com/fatlotus/lsda-control-panel.git /control-panel
+pip install -r /control-panel/requirements.txt
 
 /etc/init.d/apache2 restart || /etc/init.d/apache2 start
