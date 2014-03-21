16fad8d8a710f784c2d05644bf9751fb5a78af74
Jeremy Archer <open-source@fatlotus.com>
2014-03-20 21:57:58 -0500

    Allow LSDA to mount /mnt.

--- servers/worker.sh
+++ servers/worker.sh
@@ -27,6 +27,8 @@ git clone https://github.com/fatlotus/lsda-data-access-layer.git dal
 # Allow lsda to jump into the sandbox.
 cat > /etc/sudoers.d/lsda <<EOF
 lsda ALL=(root) NOPASSWD: /worker/sandbox.py *
+lsda ALL=(root) NOPASSWD: /bin/chown lsda:lsda /mnt
+lsda ALL=(root) NOPASSWD: /bin/mount /mnt
 EOF
 chmod 0440 /etc/sudoers.d/lsda
 
