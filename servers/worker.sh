1941afd44718d051ffdc9347c10174e144bdde8e
Jeremy Archer <open-source@fatlotus.com>
2014-03-21 14:14:41 -0500

    Further simplify sudoers file.

--- servers/worker.sh
+++ servers/worker.sh
@@ -15,7 +15,7 @@ useradd -m -r sandbox || true
 # Allow lsda to jump into the sandbox.
 cat > /etc/sudoers.d/lsda <<EOF
 lsda ALL=(root) NOPASSWD: /worker/sandbox.py *
-lsda ALL=(root) NOPASSWD: /bin/chown lsda\:lsda /mnt
+lsda ALL=(root) NOPASSWD: /bin/chown lsda /mnt
 lsda ALL=(root) NOPASSWD: /bin/mount /mnt
 EOF
 chmod 0440 /etc/sudoers.d/lsda
