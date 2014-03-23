100450fac2ed61dccde94a82801910c9211681c5
Jeremy Archer <open-source@fatlotus.com>
2014-03-23 14:13:32 -0500

    Remove per-client connection limit.

--- servers/zookeeper.sh
+++ servers/zookeeper.sh
@@ -19,6 +19,7 @@ initLimit=10
 syncLimit=5
 dataDir=/var/lib/zookeeper
 clientPort=2181
+maxClientCnxns=0
 # server.1=zookeeper1:2888:3888
 # server.2=zookeeper2:2888:3888
 # server.3=zookeeper3:2888:3888
